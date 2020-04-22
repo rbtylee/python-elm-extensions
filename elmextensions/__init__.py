c126c31951e8a8d41122cf641836d818a4532a80
Robert Wiley <ylee@bodhilinux.com>
2020-04-22 15:23:11 -0400

    Deal with __init__

--- elmextensions/__init__.py
+++ elmextensions/__init__.py
@@ -3,13 +3,13 @@ from .embeddedterminal import *
 from .aboutwindow import *
 from .fileselector import *
 from .tabbedbox import *
-from .StandardButton import *
-from .StandardPopup import *
-from .SearchableList import *
+from .standardbutton import *
+from .standardpopup import *
+from .searchablelist import *
 
-__copyright__  = "Copyright 2015-2017 Jeff Hoogland"
+__copyright__  = "Copyright 2015-2020"
 __license__    = "BSD-3-clause"
 
 # the version number: major, minor, micro, releaselevel, and serial.
-__version__    = "0.2.1rc.2"
+__version__    = "0.3.1rc.0"
 version_string = __version__
