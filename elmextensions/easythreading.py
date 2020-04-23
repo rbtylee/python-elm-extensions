6d6ff8ff33e20d77973b393a31594c553759ce88
Robert Wiley <ylee@bodhilinux.com>
2020-04-23 17:18:23 -0400

    easythreading.py: py3

--- elmextensions/easythreading.py
+++ elmextensions/easythreading.py
@@ -1,45 +1,48 @@
+# pylint: disable=c-extension-no-member
+''' easythreading a simple threaded function implementation.'''
+import threading
+import queue as Queue
 from efl import ecore
 
-import threading
-try:
-    import Queue
-except:
-    import queue as Queue
 
 class ThreadedFunction(object):
+    ''' A simple Threaded function class.'''
+
     def __init__(self, doneCB=None):
-        # private stuff
-        self._commandQueue = Queue.Queue()
-        self._replyQueue = Queue.Queue()
-        self._doneCB = doneCB
+        self._command_queue = Queue.Queue()
+        self._reply_queue = Queue.Queue()
+        self._done = doneCB
 
         # add a timer to check the data returned by the worker thread
-        self._timer = ecore.Timer(0.1, self.checkReplyQueue)
+        self._timer = ecore.Timer(0.1, self.check_reply)
 
         # start the working thread
-        threading.Thread(target=self.threadFunc).start()
+        threading.Thread(target=self.thread_func).start()
 
     def run(self, action):
-        self._commandQueue.put(action)
+        '''Just do it'''
+        self._command_queue.put(action)
 
     def shutdown(self):
+        '''Cleanup and shutdown'''
         self._timer.delete()
-        self._commandQueue.put('QUIT')
+        self._command_queue.put('QUIT')
 
-    def checkReplyQueue(self):
-        if not self._replyQueue.empty():
-            result = self._replyQueue.get()
-            if callable(self._doneCB):
-                self._doneCB()
+    def check_reply(self):
+        '''Check for reply'''
+        if not self._reply_queue.empty():
+            self._reply_queue.get()
+            if callable(self._done):
+                self._done()
         return True
 
-    # all the member below this point run in the thread
-    def threadFunc(self):
+    def thread_func(self):
+        ''' A simple threaded function.'''
         while True:
             # wait here until an item in the queue is present
-            func = self._commandQueue.get()
+            func = self._command_queue.get()
             if callable(func):
                 func()
             elif func == 'QUIT':
                 break
-            self._replyQueue.put("done")
+            self._reply_queue.put("done")
