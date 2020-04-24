4caafb62e2f24f3d3a47d89874b2eb996a291980
Robert Wiley <ylee@bodhilinux.com>
2020-04-24 10:13:14 -0400

    test_embeddedterminal.py: py3 conversion

--- test_embeddedterminal.py
+++ test_embeddedterminal.py
@@ -1,32 +1,32 @@
-import random
-
+'''Test embedded terminal'''
+# pylint: disable=no-name-in-module,no-member
 import efl.elementary as elm
-elm.init()
 from efl.elementary.window import StandardWindow
-from efl.elementary.label import Label
-from efl.elementary.button import Button
 from efl.evas import EVAS_HINT_EXPAND, EVAS_HINT_FILL
-
 from elmextensions import EmbeddedTerminal
 
 EXPAND_BOTH = EVAS_HINT_EXPAND, EVAS_HINT_EXPAND
 FILL_BOTH = EVAS_HINT_FILL, EVAS_HINT_FILL
 
-class MainWindow(object):
-    def __init__( self ):
+
+# pylint: disable=too-few-public-methods
+class MainWindow:
+    '''build main window'''
+
+    def __init__(self):
         win = StandardWindow("Testing", "Elementary Embedded Terminal")
         win.callback_delete_request_add(lambda o: elm.exit())
-
-        term = EmbeddedTerminal(win, size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
-        
+        term = EmbeddedTerminal(win,
+                                size_hint_weight=EXPAND_BOTH,
+                                size_hint_align=FILL_BOTH)
         term.show()
-        
         win.resize_object_add(term)
-
         win.resize(600, 400)
         win.show()
 
+
 if __name__ == "__main__":
+    elm.init()
     GUI = MainWindow()
     elm.run()
     elm.shutdown()
