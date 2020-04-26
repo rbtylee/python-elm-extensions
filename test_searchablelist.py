3ac13d961e956829539658bd8f97260c6bd4e046
Robert Wiley <ylee@bodhilinux.com>
2020-04-26 16:14:24 -0400

    searchablelist.py: py3 conversion

--- test_searchablelist.py
+++ test_searchablelist.py
@@ -1,32 +1,39 @@
+''' Code to test SearchableList class'''
 import efl.elementary as elm
 from efl.elementary.window import StandardWindow
-elm.init()
+# pylint: disable=no-name-in-module
 from efl.evas import EVAS_HINT_EXPAND, EVAS_HINT_FILL
+from elmextensions import SearchableList
 
 EXPAND_BOTH = EVAS_HINT_EXPAND, EVAS_HINT_EXPAND
 FILL_BOTH = EVAS_HINT_FILL, EVAS_HINT_FILL
 
-from elmextensions import SearchableList
-
+# pylint: disable=too-few-public-methods
 class MainWindow(object):
-    def __init__( self ):
+    '''Main Window for test'''
+    def __init__(self):
         win = StandardWindow("Testing", "Elementary SearchableList")
+        # pylint: disable=no-member
         win.callback_delete_request_add(lambda o: elm.exit())
-        win.show()
-        
-        ourList = SearchableList(win, size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
-        self.keys = ["Jeff", "Kristi", "Jacob", "Declan", "Joris", 
-                "Timmy", "Tristam"]
+
+        our_list = SearchableList(win,
+                                  size_hint_weight=EXPAND_BOTH,
+                                  size_hint_align=FILL_BOTH)
+        self.keys = [
+            "Jeff", "Kristi", "Jacob", "Declan", "Joris", "Timmy", "Tristam"
+        ]
         for kbl in self.keys:
-            ourList.item_append(kbl)
-        ourList.show()
-        
-        win.resize_object_add(ourList)
+            our_list.item_append(kbl)
+        our_list.show()
+
+        win.resize_object_add(our_list)
 
         win.resize(600, 400)
         win.show()
 
+
 if __name__ == "__main__":
     GUI = MainWindow()
+    # pylint: disable=no-member
     elm.run()
     elm.shutdown()
